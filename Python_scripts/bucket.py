c6a5d3106bc699f8244ea063dffcd1ed3492fb36
Rohan Bansal <robansal@deloitte.com>
2022-01-07 15:13:58 +0530

    test23

--- Python_scripts/bucket.py
+++ Python_scripts/bucket.py
@@ -1,8 +1,9 @@
 import os
-name_of_service = os.environ['name_of_bucket']
+name_of_bucket = os.environ['name_of_bucket']
 region = os.environ['region']
 
-print("bucket gcp")
-print("Name is  :",name_of_service)
-print("Region is", region)
+# print("bucket gcp")
+# print("Name is  :",name_of_service)
+# print("Region is", region)
 
+print("Storage Bucket with name",name_of_bucket,"is created in region",region)
