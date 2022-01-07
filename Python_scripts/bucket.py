6c20b626a4e9ae40c5b2ef37c5716ccbb5311722
Rohan Bansal <robansal@deloitte.com>
2022-01-07 16:22:07 +0530

    sample1

--- Python_scripts/bucket.py
+++ Python_scripts/bucket.py
@@ -1,9 +1,48 @@
+# import os
+# name_of_bucket = os.environ['name_of_bucket']
+# region = os.environ['region']
+
+# # print("bucket gcp")
+# # print("Name is  :",name_of_service)
+# # print("Region is", region)
+
+# print("Storage Bucket with name",name_of_bucket,"is created in region",region)
+
+
+
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Create a new Cloud Storage bucket on GCP.
+
 import os
-name_of_bucket = os.environ['name_of_bucket']
-region = os.environ['region']
+from google.cloud import storage
+from google.cloud.exceptions import Conflict
 
-# print("bucket gcp")
-# print("Name is  :",name_of_service)
-# print("Region is", region)
+def main():
+    name_of_bucket = os.environ['name_of_bucket']
+    region = os.environ['region']
 
-print("Storage Bucket with name",name_of_bucket,"is created in region",region)
+    print('Bucket name to be created is : ' + name_of_bucket)
+    print('Creating bucket ...')
+    
+    # Instantiate the client.
+    client = storage.Client()
+
+    try:
+        # Instantiate the bucket.
+        bucket = client.bucket(name_of_bucket)
+
+        # Create the bucket object.
+        bucket.create(location=region)
+        print('\nCreated')
+        
+    except Conflict:
+        print('Error: Bucket already exists!!')
+        pass
+  
+    return
+  
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+    main()
