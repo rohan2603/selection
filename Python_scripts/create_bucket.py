682bacf716217e22c3aeb11c1ef0cbcc5f4d4eb2
Rohan Bansal <robansal@deloitte.com>
2022-01-10 19:34:56 +0530

    t1

--- Python_scripts/create_bucket.py
+++ Python_scripts/create_bucket.py
@@ -1,16 +1,3 @@
-# import os
-# name_of_bucket = os.environ['name_of_bucket']
-# region = os.environ['region']
-
-# # print("bucket gcp")
-# # print("Name is  :",name_of_service)
-# # print("Region is", region)
-
-# print("Storage Bucket with name",name_of_bucket,"is created in region",region)
-
-
-
-
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Create a new Cloud Storage bucket on GCP.
@@ -19,19 +6,13 @@ import os
 from google.cloud import storage
 from google.cloud.exceptions import Conflict
 
-#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../credentials.json"
-
 def main():
-
     name_of_bucket = os.environ['name_of_bucket']
     region = os.environ['region']
-
     print('Bucket name to be created is : ' + name_of_bucket)
     print('Creating bucket ...')
-    
     # Instantiate the client.
     client = storage.Client()
-
     try:
         # Instantiate the bucket.
         bucket = client.bucket(name_of_bucket)
@@ -43,9 +24,9 @@ def main():
     except Conflict:
         print('Error: Bucket already exists!!')
         pass
-  
+
     return
-  
+      
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
     main()
