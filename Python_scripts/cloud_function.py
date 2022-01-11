267db6a12610337385d00c399b363303fcba053a
Rohan Bansal <robansal@deloitte.com>
2022-01-11 12:24:18 +0530

    test5

--- Python_scripts/cloud_function.py
+++
@@ -1,9 +0,0 @@
-import os
-name_of_cloud_function = os.environ['name_of_cloud_function']
-region = os.environ['region']
-
-# print("bucket gcp")
-# print("Name is  :",name_of_service)
-# print("Region is", region)
-
-print("Cloud Function with name",name_of_cloud_function,"is created in region",region)
